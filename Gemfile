8d345840dceac9c974b27ff566dd62a2fe584432
vgulaev <vgulaev@yandex.ru>
2020-04-19 23:06:20 +0500

    Up

--- Gemfile
+++ Gemfile
@@ -52,4 +52,3 @@ end
 gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'pg'
-gem 'rspec-rails'
--- test/controllers/api_controller_test.rb
+++ test/controllers/api_controller_test.rb
@@ -2,13 +2,12 @@ require 'test_helper'
 
 class ApiControllerTest < ActionDispatch::IntegrationTest
   test "should get server" do
-    # get api_server_url
-    # assert_response :success
+    get api_server_url
+    assert_response :success
   end
 
   test "should get bandwidth" do
-    # get api_bandwidth_url
-    # assert_response :success
+    get api_bandwidth_url
+    assert_response :success
   end
-
 end
